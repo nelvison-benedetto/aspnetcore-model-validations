901df91c9fadc69bc67293cdc79c37e54efa4574
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-01 22:27:51 +0100

    update

---
+++ CustomValidators/MinimumYearValidatorAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _5_aspnetcore_model_validations.CustomValidators;
+
+public class MinimumYearValidatorAttribute : ValidationAttribute
+{
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        if (value != null) {
+            DateTime date = (DateTime)value;
+            if (date.Year >= 2000)
+            {
+                return new ValidationResult("minimum year allowed is 2000");
+            }
+            else { 
+                return ValidationResult.Success;
+            }
+        }
+    }
+
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,15 +1,33 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace _5_aspnetcore_model_validations.Models;
 
 public class Person
 {
     [Required(ErrorMessage ="person name can't be empty or null")]
+    [Display(Name ="Person Name")]
+    [StringLength(40,MinimumLength =3, ErrorMessage ="{0} should be between {2 and {1} chars long")]
+    [RegularExpression("^[A-Za-z .]$", ErrorMessage="{0} should contain only alphabets space and dot (.)")]
     public string? PersonName { get; set; }
+
+    [EmailAddress(ErrorMessage ="{0} should be a proper email address")]
     public string? Email { get; set; }
+
+    [Phone(ErrorMessage ="{0} should contain 10 digits.")]
+    //[ValidateNever]
     public string? Phone { get; set; }
+
+    [Required(ErrorMessage ="{0} can't be blank")]
     public string? Psw { get; set; }
+
+    [Required(ErrorMessage = "{0} can't be blank")]
+    [Compare("Psw", ErrorMessage ="{0} and {1} don't match")]
+    [Display(Name ="re-enter psw")]
     public string? ConfirmPsw { get; set; }
+
+    [Range(0, 999, ErrorMessage ="{0} should be between {1} and {2}")]
     public double? Price { get; set; }
 
     public override string ToString()
