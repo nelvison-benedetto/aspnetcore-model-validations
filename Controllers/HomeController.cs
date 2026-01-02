d951e957884d49b71e01ce4344ecfe824f8e7417
nelvisonbenedetto <nelvison.benedetto@gmail.com>
2026-01-02 12:59:19 +0100

    done

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,12 +8,39 @@ public class HomeController : ControllerBase
     [Route("register")]
     public IActionResult Index(
     //[Bind(nameof(Person.PersonName), nameof(Person.Email), nameof(Person.Psw), nameof(Person.ConfirmPsw) ) ] , aspnetcore fa sempre in auto il binding di tutti i fields public, se non vuoi che target fields sia bindato allora su field [BindNever]
-    Person person) {
-        if(!ModelState.IsValid) {
+    [FromBody]Person person, [FromHeader(Name = "User-Agent")] string UserAgent )
+    {  // da body prendi i dati e fai il model binding su person, anche senza scrivere [FromBody] esso è implicito w apicontroller,  da header prendi User-Agent e bindalo su string UserAgent
+        if (!ModelState.IsValid) {
             string errors = string.Join("\n", ModelState.Values.SelectMany(value => value.Errors).Select(err => err.ErrorMessage));
             return BadRequest(errors);
         }
-        return Content($"{person}");
+        return Content($"{person}, {UserAgent}");
     }
 
+    [HttpGet("search")]  //è Route+GET, Route da solo non va bene puo rispondere anche a post/put/delete/ect
+    public IActionResult Search(
+    [FromQuery] string? name,
+    [FromQuery] int? minAge,
+    [FromQuery] int? maxAge
+)
+    {
+        return Ok(new
+        {
+            name,
+            minAge,
+            maxAge
+        });
+    }
+    //GET /search?name=nevil&minAge=16&maxAge=46
+    //w [FromQuery] binding auto di name, minAge,maxAge dai query string params
+
+    [HttpGet("person/{id}")]
+    public IActionResult GetPerson(
+    [FromRoute] int id
+)
+    {
+        return Ok($"Requested person id = {id}");
+    }
+    //GET /person/42
+    //w [FromRoute] binding auto di id dal route url
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace _5_aspnetcore_model_validations
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddXmlSerializerFormatters();  //add xml formatter x input/output, di default aspnetcore supporta solo json, ora supporta anche application/xml text/xml
             var app = builder.Build();
 
             app.UseStaticFiles();
@@ -13,6 +13,9 @@ namespace _5_aspnetcore_model_validations
             app.MapControllers();
 
             app.Run();
+
+            //lesson 76-77 Custom Binding e come apply, non cosnigliato usarlo spesso, no code here.
+
         }
     }
 }
